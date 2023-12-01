f1a362fd65779402d3c90a6f847fe736cdb9e6da
khrlamin <152362419+khrlamin@users.noreply.github.com>
2023-12-01 17:51:18 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
+ */
+
+var typed = new Typed(".multiple-text", {
+    strings: ["Web Developer", "Game Developer", "Programmer"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+})
